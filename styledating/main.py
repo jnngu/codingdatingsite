6a97b2c62c43975b6d3ecd3da23a86d84184d5a1
gabrielchuang <gabriel.t.chuang@gmail.com>
2021-03-06 16:16:04 -0500

    proper logging in

--- styledating/main.py
+++ styledating/main.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from flask_login import LoginManager, UserMixin, login_required, login_user, logout_user, current_user
 import sqlite3
 import hashlib
 from util import *
+from user import *
 
 app = Flask(__name__)
+app.secret_key = open('secret_key.txt').read()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
 
 @app.route('/')
 def index():
@@ -29,12 +39,13 @@ def process_registration():
 		(newid,f['username'],f['name'],'',hashpw(f['pass']),))
 	pfp = request.files['pfp']
 	pfp.save('database/pfps/'+f['username'])
-	return f"registered! You are user #{newid}<br>" + render_template("login.html")
+	#return f"registered! You are user #{newid}<br>" + render_template("login.html")
+	return redirect(url_for('login'))
+
 
 @app.route('/login')
 def login():
 	return render_template("login.html")
-
 
 @app.route('/process_login', methods=['POST'])
 def process_login():
@@ -44,23 +55,36 @@ def process_login():
 	print(L)
 	if len(L) == 0 or L[0][0] == None:
 		return "sorry, login failed\n" + render_template("login.html")
-	return f"success! welcome, {L[0][0]}"
+	user = User(f['username'])
+	login_user(user)
+	return redirect(url_for('coding_chal'))
 
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
 
 @app.route('/code')
+@login_required
 def coding_chal():
 	return render_template("formpg.html")
 
 @app.route('/process_code', methods=['POST'])
+@login_required
 def process_code():
 	user_code = request.form['code_submission']
 	return "your code was " + user_code
 
 @app.route('/match')
+@login_required
 def match():
-	return "here are your matches"
+	print(current_user)
+	print(current_user.id)
+	return f"hi {current_user.name}, here are your matches"
 
 @app.route('/chat')
+@login_required
 def chat():
 	return "welcome to chat"
 
